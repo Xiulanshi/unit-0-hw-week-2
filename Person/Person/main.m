3aa18e21e2990f181762c8b1629153c4fc236511
Xiulan Shi <xiulanshi2013@gmail.com>
2015-06-23 19:16:50 -0400

    changed my readme file and Person main

--- Person/Person/main.m
+++ Person/Person/main.m
@@ -2,9 +2,13 @@
 //  main.m
 //  Person
 //
-//  Created by Michael Kavouras on 6/21/15.
-//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
-//
+//  Created by Xiulan Shi on 6/22/15.
+//  Copyright (c) 2015 Xiulan Shi. All rights reserved.
+//  Code by Xiulan Shi
+/*
+ Write a method called checkSameCity which accepts one parameter of type Person * and checks if they live in the same city. The method should return a boolean value.
+ A Person has recently had a child, whose name is 'Abc'. Write a method called registerChild which takes 0 parameters and returns a new Person * instance represeting the child, which has the same phoneNumber and city as the parent.
+ */
 
 #import <Foundation/Foundation.h>
 
@@ -18,6 +22,12 @@
 
 - (void)setPhoneNumber:(NSString *)phoneNumber;
 - (NSString *)phoneNumber;
+
+// create a method called checkSameCity and it would return a boolean value (BOOL).
+- (BOOL)checkSameCity:(Person *)person;
+
+// create a method called registerChild which takes 0 parameters and returns a new Person * instance.
+- (Person *)registerChild;
 
 @end
 
@@ -51,13 +61,61 @@
     return _phoneNumber;
 }
 
+//define the method checkSameCity (give the actual code). start by the type of method (instance), its return type (BOOL), and its arguments and their types.
+- (BOOL)checkSameCity:(Person *)person {
+    //if they live in the same city, it will print out YES and return YES. Otherwise, NO.
+    if([_city isEqualToString:[person city]]) {
+        NSLog(@"YES");
+        return YES;
+    } else {
+        NSLog(@"NO");
+        return NO;
+    }
+}
+
+//define the method registerChild. start by the type of method (instance), its return type (Person *), and it's arguments and their types.
+- (Person *)registerChild {
+    //create an instance of a Person called child and initialize it
+    Person *child = [[Person alloc] init];
+    
+    //setName to "Abc"
+    [child setName:@"Abc"];
+    //setPhoneNumber to the parent's phoneNumber
+    [child setPhoneNumber:_phoneNumber];
+    //setCity to the parent's city
+    [child setCity:_city];
+    
+    //print out the child's name, phonenumber and city by using NSLog
+    NSLog(@"The Child's name is %@. Phone number is %@. City is %@.",child.name, child.phoneNumber, child.city);
+    return child;
+}
 @end
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //create an instance of a Person called personA and initialize it
+        Person *personA = [[Person alloc] init];
+        
+        //setName, setCity and setPhoneNumber for personA
+        [personA setName:@"John"];
+        [personA setCity:@"NewYork"];
+        [personA setPhoneNumber:@"7188202805"];
+        
+        //create an instance of a Person called personB and initialize it
+        Person *personB = [[Person alloc] init];
+        
+        //setName, setCity and setPhoneNumber for personB
+        [personB setName:@"Angel"];
+        [personB setCity:@"NewYork"];
+        [personB setPhoneNumber:@"7188202805"];
+        
+        //use checkSameCity to check if they live in the same city.
+        [personB checkSameCity:personA];
+    
+        //use registerChild to print out the child's name, phone number and city.
+        [personA registerChild];
+        
     }
     return 0;
 }
